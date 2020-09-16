ac9f94b0a1cdd3d488e4e8d52f0fcb9ce5bc35f5
Bharath <crbharath4@gmail.com>
2020-09-16 16:36:39 +0530

    Done With the project

---
+++ app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.videoplayer;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView videoView = (VideoView)findViewById(R.id.vdVw);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        Uri uri = Uri.parse("andriod.resource://" + getPackageName() + "/" + R.raw.video1);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+    }
+}
